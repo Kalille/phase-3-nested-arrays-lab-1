1dd0e99d06a84941931e8bc305568eb1973edb8b
kalille Overton <>
2021-08-29 14:53:43 -0400

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -26,13 +26,13 @@ newArr
 end
 
 def sorted_matrix
-  newArr =assembled_matrix
-  assembled_matrix[0].sort + assembled_matrix[1].sort
+  
+  assembled_matrix[0].sort 
   
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
-  assembled_matrix
+  
 
 end
 
